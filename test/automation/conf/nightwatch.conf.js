876495a17f8042676d0a45e20b7e9a0d53ba30e2
Michael Roberts <michael@origincodeacademy.com>
2019-02-23 18:38:24 -0800

    configured to use docker compose

--- test/automation/conf/nightwatch.conf.js
+++ test/automation/conf/nightwatch.conf.js
@@ -1,19 +1,61 @@
+const seleniumServer = require('selenium-server');
+const chromedriver = require('chromedriver');
+const geckodriver = require('geckodriver');
+
 module.exports = {
-    src_folders: ["test/automation/tests"],
-    selenium: {
-      start_process: true,
-      server_path: require("selenium-server").path,
-      host: "127.0.0.1",
-      port: 4444,
-      cli_args: {
-        "webdriver.chrome.driver": require("chromedriver").path
+  src_folders: ['test/automation/tests'],
+  output_folder: 'reports',
+  custom_assertions_path: '',
+  live_output: false,
+  disable_colors: false,
+  selenium: {
+    start_process: true,
+    server_path: seleniumServer.path,
+    log_path: '',
+    host: '127.0.0.1',
+    port: 4444
+  },
+
+  test_settings: {
+    default: {
+      screenshots: {
+        enabled: false,
+        path: '',
+        on_failure: true,
+        on_error: true
+      },
+      launch_url: 'http://localhost',
+      selenium_port: 4446,
+      selenium_host: '127.0.0.1',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true
       }
     },
-    test_settings: {
-      default: {
-        desiredCapabilities: {
-          browserName: "chrome"
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true
+      },
+      selenium: {
+        cli_args: {
+          'webdriver.chrome.driver': chromedriver.path
+        }
+      }
+    },
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true,
+        marionette: true
+      },
+      selenium: {
+        cli_args: {
+          'webdriver.gecko.driver': geckodriver.path
         }
       }
     }
-  };
+  }
+};
